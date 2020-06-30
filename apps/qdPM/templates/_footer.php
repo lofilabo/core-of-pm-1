b7880349f09198a7fa27a1896fb9cfc8456cf402
Richard <richardlofi@gmail.com>
2020-07-01 00:03:26 +0100

    cright2

--- apps/qdPM/templates/_footer.php
+++ apps/qdPM/templates/_footer.php
@@ -5,7 +5,7 @@
 <!-- BEGIN FOOTER -->
 <div class="footer">
 <div class="footer-inner">
-	 <a href="http://p4pms" target="_blank">qdPM <?php echo sfConfig::get('app_qdpm_version') ?></a> <br> Copyright &copy; <?php echo  date('Y') ?> <a href="http://p4pms" target="_blank">p4pms</a>
+	 <a href="http://p4pms" target="_blank">p4pms <?php echo sfConfig::get('app_qdpm_version') ?></a> <br> Copyright &copy; <?php echo  date('Y') ?> <a href="http://p4pms" target="_blank">p4pms</a>
 </div>
 <div class="footer-tools">
 	<span class="go-top">
